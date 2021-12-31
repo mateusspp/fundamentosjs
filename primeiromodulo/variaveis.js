ea9e4c566cd0077d9d4b66e4936ba9edb489e3d9
mateusspp <teu.patricio@hotmail.com>
2021-12-31 00:29:21 -0300

    Adicionando exemplos de variaveis

---
+++ primeiromodulo/variaveis.js
@@ -0,0 +1,17 @@
+// Exemplo com a atuação das três variáveis primitivas
+
+const forma = "triângulo" ;
+const altura = 5;
+const comprimento = 7;
+let area; 
+
+if (forma === "quadrado") {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area);
+
+// Esse exemplo, temos a const (valor imutável), let (pode ser mudado, mas deve ser declarado antes) e por fim, area dentro do if é uma var, fora é let, mas dentro do if
+// por padrão, qualquer variável é var
