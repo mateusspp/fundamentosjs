c6e213e4f5b8ff850889b9581cb947453b67cee6
mateusspp <teu.patricio@hotmail.com>
2021-12-31 21:54:32 -0300

    truthy and falsy

--- primeiromodulo/truthy.js
+++ primeiromodulo/truthy.js
@@ -1,3 +1,9 @@
 // São situações, elementos ou paramentos que são indentificados ou servem como true ou false
 
-// São cham
+// São chamados de truthy ou falsy
+
+const vazio = "";
+const numero = 0;
+const verdadeiro = 1;
+
+console.log(vazio == true);
