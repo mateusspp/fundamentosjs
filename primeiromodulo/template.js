a84d3c70d5ae6dce167390d9ca80d9690cb78ef6
mateusspp <teu.patricio@hotmail.com>
2022-01-01 22:56:33 -0300

    Template Strings, exemplos

---
+++ primeiromodulo/template.js
@@ -0,0 +1,9 @@
+// Exemplo de template strings
+
+const nome = "Ju";
+const idade = 2021-1981;
+const cidade = "Rio de Janeiro";
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidade}`;
+
+console.log(apresentacao);
