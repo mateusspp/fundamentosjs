43f37fedd2382523c3aa7a3ddb1f45c9e0c32df2
mateusspp <teu.patricio@hotmail.com>
2022-01-01 22:39:58 -0300

    Operador ternario

---
+++ primeiromodulo/ternario.js
@@ -0,0 +1,17 @@
+// Exemplo de Operador Ternário lá embaixo
+
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+// if (idadeCliente >= idadeMinima) {
+//     console.log("cerveja")
+// }
+//     else {
+//         console.log("suco")
+//     };
+
+
+// Aqui o exemplo, o nome ternário:
+
+               //condiçção                    //true      //false
+    console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco");
