3ecba382cb2b850542cf391dfcd294633c43f15c
mateusspp <teu.patricio@hotmail.com>
2022-01-03 22:49:31 -0300

    Expressao fim

--- primeiromodulo/expressao.js
+++ primeiromodulo/expressao.js
@@ -8,3 +8,16 @@ minhaFuncao("param");
 
 const soma = function(num1, num2) {return num1 + num2};
 console.log(soma(1, 1));
+
+
+// Diferenca das formas de escrever funções
+console.log(apresentar())
+
+function apresentar () {
+    return "olá";
+}
+
+
+//Isso vai dá erro
+console.log(soma(1,1));
+const soma = function(num1, num2) { return num1 + num2};
