902cbf6a8d1d76216237d63688dfb3806040c033
mateusspp <teu.patricio@hotmail.com>
2022-01-04 19:44:16 -0300

    Exemplo arrow

--- primeiromodulo/arrow.js
+++ primeiromodulo/arrow.js
@@ -7,4 +7,12 @@ const apresentarArrow = nome => `meu nome é ${nome}`;
 const soma = (num1, num2) => num1 + num2;
 
 //Arrow function com + de 1 linha de instrução
-const somaNumerosPequenos = (num1, num2) => 
+const somaNumerosPequenos = (num1, num2) => {
+    if (num1 || num2 > 10) {
+        return "somente números de 1 a 9"
+    } else {
+        return num1 + num2
+    }
+}
+
+//Hoisting: arrow function se comporta como expressão
