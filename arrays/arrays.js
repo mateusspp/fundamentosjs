98d9eb4a3395ebde7f004fe22424c0b2520d1d4a
mateusspp <teu.patricio@hotmail.com>
2022-01-10 15:49:13 -0300

    Array exemplo simples

---
+++ arrays/arrays.js
@@ -0,0 +1,8 @@
+// Exemplo de um array simples:
+
+            //0    1    2   3 
+const notas = [10, 6.5, 8, 7,5];
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media)
