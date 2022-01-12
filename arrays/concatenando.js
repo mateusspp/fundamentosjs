ee9285f5f954741aba4a1b56382801c406ee2670
mateusspp <teu.patricio@hotmail.com>
2022-01-11 21:06:23 -0300

    Exemplo concatenando

---
+++ arrays/concatenando.js
@@ -0,0 +1,9 @@
+// Concatenando Arrays
+
+const salaDePython = ["Melissa", "Helena", "Rodrigo"];
+
+const salaDeJavaScript = ["Ju", "Leo", "Raquel"];
+
+const salaUnificados = salaDePython.concat(salaDeJavaScript);
+
+console.log(salaUnificados);
