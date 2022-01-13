17a12f9fc0c5d668f2fa86595ca6adf7b671d2eb
mateusspp <teu.patricio@hotmail.com>
2022-01-12 22:22:15 -0300

    Exemplo de for

---
+++ arrays/for.js
@@ -0,0 +1,8 @@
+// Exemplo de for
+
+const numeros = [100, 200, 300, 400, 500, 600];
+          // 2      6
+for (let i = 0; i < numeros.length; i++) {
+                // numeros [0]
+    console.log(i, numeros[i]);
+}
