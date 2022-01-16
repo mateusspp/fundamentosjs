15e45fa42c79eb566167119e68cf0e31f654fc4a
mateusspp <teu.patricio@hotmail.com>
2022-01-15 22:12:11 -0300

    Filtrando elementos com filter

---
+++ arrays/filtrandoelementos.js
@@ -0,0 +1,10 @@
+// Exemplo de filtração de elementos
+
+const nomes = ["Ana", "Marcos", "Maria", "Mauro"];
+const notas = [7, 4.5, 8, 7.5];
+
+const reprovados = nomes.filter((aluno,indice) => notas [indice] < 5);
+
+// O parâmetro indice não é obrigatório
+
+console.log(`reprovados: ${reprovados}`);
