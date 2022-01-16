70d35e33c8146f55fdd55717c9ac14989a184f11
mateusspp <teu.patricio@hotmail.com>
2022-01-15 21:58:09 -0300

    Exemplo de alteraçao de array utilizando o map

---
+++ arrays/alterandomap.js
@@ -0,0 +1,7 @@
+// Exemplo de alteração de strings com map()
+
+let nomes = ["ana Julia", "Caio vinicius", "Bia silva"];
+
+const nomesAtualizados = nomes.map(nome => nome.toUpperCase());
+
+console.log(nomesAtualizados);
