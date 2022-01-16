763f3522a19ea37ddd25f12ab264dca6be7dc045
mateusspp <teu.patricio@hotmail.com>
2022-01-16 00:27:50 -0300

    Exemplo da estrutura de um objeto

---
+++ objetos/objetos.js
@@ -0,0 +1,10 @@
+// Exemplo de objeto
+
+// const cliente = ["nome", "Andŕe", "idade", 36];
+
+const cliente = {
+    nome:"André",
+    idade:36,
+    cpf:"12356478590",
+    email:"andre@gmail.com"
+}
